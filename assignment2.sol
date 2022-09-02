8baf3e6940326028fdf22dc86ab81954c48db243
Shaheer Shahzad <mohammedshaheershahzadrawda5@gmail.com>
2022-09-02 22:17:31 +0500

    update

--- assignment2.sol
+++ assignment2.sol
@@ -3,3 +3,23 @@ contract Assignment {
          return y**x;
     }
 }
+
+
+
+function task2(uint num) public pure returns (uint){
+     uint256 reverse = 0;
+        uint256 remainder;
+        uint256 original;
+
+        original = num;
+        while (num != 0) {
+            remainder = num % 10;
+            reverse = reverse * 10 + remainder;
+          num /= 10;
+        }
+        if (original == reverse) {
+            return 1; //palindrome
+        } else {
+            return 0; //not palindrome
+        }
+}
